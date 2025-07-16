7762215670c72d64c38d5c89c1745c2ffe561030
trananh-tt-intervideo <32162425+sammytran90@users.noreply.github.com>
2025-07-17 01:33:14 +0700

    OrbitControl settings

--- src/components/canvas/View.tsx
+++ src/components/canvas/View.tsx
@@ -7,10 +7,16 @@ import { Three } from '@/helpers/components/Three'
 export const Common = ({ color }: { color: string }) => (
   <Suspense fallback={null}>
     {color && <color attach='background' args={[color]} />}
-    <ambientLight />
-    <pointLight position={[20, 30, 10]} intensity={3} decay={0.2} />
-    <pointLight position={[-10, -10, -10]} color='blue' decay={0.2} />
-    <PerspectiveCamera makeDefault fov={40} position={[0, 0, 6]} />
+    <ambientLight intensity={0.5} />
+    <directionalLight position={[-2.8, 1.8, -0.7]} intensity={3} />
+    <PerspectiveCamera
+      makeDefault
+      fov={80}
+      aspect={window.innerWidth / window.innerHeight}
+      near={0.1}
+      far={1000}
+      position={[8, 6.5, 10.9]}
+    />
   </Suspense>
 )
 
@@ -29,7 +35,7 @@ const View = forwardRef(({ children, orbit, ...props }: ViewProps, ref) => {
       <Three>
         <ViewImpl track={localRef}>
           {children}
-          {orbit && <OrbitControls />}
+          {orbit && <OrbitControls target={[0, 0, 0]} minDistance={1} maxDistance={5} rotateSpeed={1} />}
         </ViewImpl>
       </Three>
     </>
