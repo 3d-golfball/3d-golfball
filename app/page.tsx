fd7c138cf225fe1dc90e5a9d17a9a59963e49393
trananh-tt-intervideo <32162425+sammytran90@users.noreply.github.com>
2025-07-17 01:34:38 +0700

    fix lint errors

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.
   ssr: false,
   loading: () => (
     <div className='flex h-96 w-full flex-col items-center justify-center'>
-      <svg className='-ml-1 mr-3 h-5 w-5 animate-spin text-black' fill='none' viewBox='0 0 24 24'>
+      <svg className='-ml-1 mr-3 size-5 animate-spin text-black' fill='none' viewBox='0 0 24 24'>
         <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4' />
         <path
           className='opacity-75'
@@ -24,7 +24,7 @@ const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mo
 
 export default function Page() {
   return (
-    <div id='golf-ball' className='relative my-12 h-[100vh] w-full py-6'>
+    <div id='golf-ball' className='relative my-12 h-screen w-full py-6'>
       <View orbit className='relative h-full'>
         <Suspense fallback={null}>
           <GolfBallOBJ scale={1} position={[0, 0, 0]} rotation={[0, 0, 0]} />
